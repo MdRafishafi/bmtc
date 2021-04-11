d0caed6dbc77b94ae980efa53200a0e511da1228
G Mohammad Rafi <shafi20014@gmail.com>
2021-04-11 14:24:05 +0530

    fixed all error

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ db.create_all()
 
 def generate_id(table_data: db.Model):
     uid = str(uuid.uuid4()).replace('-', '')
-    result = table_data.query(table_data.id).filter_by(id=uid).first()
+    result = table_data.query.filter_by(id=uid).first()
+    print(result)
     if not result:
         return uid
     else:
